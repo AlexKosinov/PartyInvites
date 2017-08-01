0149c5d316347ea224ac1df1972c4e88c5add115
AlexKosinov <kosinov2993@gmail.com>
2017-08-01 15:50:39 +0300

    Validation Form (page 41).

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Пожалуйста, введите Ваше имя")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите Ваш электронный адрес")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Пожалуйста, введите правильный электронный адрес")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите Ваш номер телефона")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, укажите будете ли Вы присутствовать")]
         public bool? WillAttend { get; set; }
     }
 }
