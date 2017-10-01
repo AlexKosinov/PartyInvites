d170f7fe812d5c88d59a116d7869d197c669d39c
AlexKosinov_home <kosinov2993@gmail.com>
2017-10-01 19:56:21 +0300

    Add the validation, smtp confirm sending. (page 45)

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,17 @@ namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Пожалуйста, введите Ваше имя")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите Ваш почтовый адрес")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Пожалуйста, введите корректный адрес")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите Ваш номер телефона")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, укажите, будете ли Вы присутствовать")]
         public bool? WillAttend { get; set; }
     }
 }
